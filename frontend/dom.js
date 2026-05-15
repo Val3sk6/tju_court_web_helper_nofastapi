d25c0a44598c734c4db77d53f91eea4b3f42fbc5
Val3sk6 <7813268@QQ.com>
2026-05-15 19:09:10 +0800

    Split frontend app into modules

---
+++ frontend/dom.js
@@ -0,0 +1,11 @@
+export const $ = (id) => document.getElementById(id);
+
+export const fieldsBox = $('fields');
+export const fieldTemplate = $('fieldTemplate');
+export const logs = $('logs');
+export const statusPill = $('statusPill');
+export const logFilter = $('logFilter');
+export const logMeta = $('logMeta');
+export const autoScroll = $('autoScroll');
+export const presetSelect = $('presetSelect');
+export const importConfigFile = $('importConfigFile');
